b910db3c3fd3039276783db9684d95dc3294e6c8
skalinin <kalinin.sa@phystech.edu>
2017-03-30 15:18:32 +0300

    editor_err.c added. printerr moved to editor_err

--- Editor/editor_utils.h
+++ Editor/editor_utils.h
@@ -21,15 +21,6 @@
 #define M_INSERT 1
 #define M_EDIT   2
 
-#define RESET   "\033[0m"
-#define RED     "\033[1;31m"
-#define GREEN   "\033[1;32m"
-#define YELLOW  "\033[1;33m"
-#define BLUE    "\033[1;34m"
-#define MAGENTA "\033[1;35m"
-#define CYAN    "\033[1;36m"
-#define WHITE   "\033[1;37m"
-
 typedef unsigned int ui32;
 
 extern size_t tab_width;
--- Editor/main.c
+++ Editor/main.c
@@ -16,59 +16,7 @@
 #include "editor_invokers.h"
 #include "editor_line.h"
 
-
-
-
 size_t tab_width = 8;
-
-
-
-
-
-/**
-  * Recieves error code and prints error message to stderr stream
-  * Recieved error code is returned
-  */
-
-
-int printerr(int err)
-{
-    switch(err)
-    {
-    case 0:
-        break;
-    case E_STRRNG:
-        fwprintf(stderr, L"Incorrect symbol position\n");
-        break;
-    case E_MALLOC:
-        fwprintf(stderr, L"Not enough memory\n");
-        break;
-    case E_LNRNG:
-        fwprintf(stderr, L"Incorrect line number\n");
-        break;
-    case E_C_WRNG:
-        fwprintf(stderr, L"Incorrect command\n");
-        break;
-    case E_NOARG:
-        fwprintf(stderr, L"Missing or invalid arguments\n");
-        break;
-    case E_NARROW:
-        fwprintf(stderr, L"Treminal\'s width not enough\n");
-        break;
-    case E_IOFAIL:
-        fwprintf(stderr, L"Input/output error has occured\n");
-        break;
-    case E_NOFILE:
-        fwprintf(stderr, L"No file specified\n");
-        break;
-    case E_NOTSAV:
-        wprintf(L"File not saved. Use %sexit force%s to exit anyway.\n", RED, RESET);
-        break;
-    default:
-        fwprintf(stderr, L"What the...");
-    }
-    return err;
-}
 
 int main(int argc, const char *argv[])
 {
