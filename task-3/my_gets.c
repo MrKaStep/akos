bd6b47d29e96ebe362057159dc20315e0ad2070c
skalinin <kalinin.sa@phystech.edu>
2017-04-19 13:15:33 +0300

    Task 3 complete (main unsafe, demo only)

--- task-3/my_gets.c
+++ task-3/my_gets.c
@@ -1,3 +1,5 @@
+#define _XOPEN_SOURCE 1000
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -5,8 +7,7 @@
 
 #define E_OK     0
 #define E_MALLOC 1
-#define E_CORPT  2
-#define E_EOF    3
+#define E_EOF    2
 
 int err = 0;
 
@@ -27,15 +28,16 @@ int expand_string(char** str, int len)
     return add;
 }
 
-int read_string(char** str, FILE* f)
+int read_string(FILE* f, char** str)
 {
-    int fd = fileno(f);
     char* s = malloc(4 * sizeof(char));
     int buf = 4;
     int sz = 0;
+    if(s == NULL)
+        return E_MALLOC;
     while(1)
     {
-        int rd;
+        int c;
         if(sz == buf - 1)
         {
             if(expand_string(&s, buf) == 0)
@@ -44,52 +46,34 @@ int read_string(char** str, FILE* f)
                 return E_MALLOC;
             }
         }
-        rd = read(fd, s + sz, sizeof(char));
-        if(rd == -1)
+        c = fgetc(f);
+        if(c == EOF)
         {
-            if(rd == EAGAIN)
-            {
-                continue;
-            }
-            perror("read_string: ");
-            *str = s;
-            return E_CORPT;
-        }
-        if(rd == 0)
-        {
-            s[sz] = 0;
-            *str = s;
-            return E_OK;
-        }
-        if(rd != sizeof(char))
-        {
-            s[sz] = 0;
-            *str = s;
-            return E_CORPT;
-        }
-        if(s[sz] == '\n')
-        {
-            s[sz] = 0;
-            *str = s;
-            return E_OK;
-        }
-        if(s[sz] == EOF)
-        {
-            s[sz] = 0;
+            s[sz] = '\0';
             *str = s;
             return E_EOF;
         }
+        s[sz] = c;
+        if(s[sz] == '\n')
+        {
+            s[sz] = '\0';
+            *str = s;
+            return E_OK;
+        }
         ++sz;
     }
-    *str = s;
-    return E_OK;
 }
 
-int main(int argc, const char** argv)
+int main(int argc, const char* argv[])
 {
     int s = 0;
     char* buf;
-    while(read_string(&buf, stdin) == E_OK)
+    FILE* f;
+    if(argc > 1)
+        f = fopen(argv[1], "r");
+    else
+        f = stdin;
+    while(read_string(f, &buf) == E_OK)
     {
         printf("%s\n", buf);
         s += 1;
