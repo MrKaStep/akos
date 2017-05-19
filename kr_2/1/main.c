f3aa2e2dd4367aac194220802cf9efc82dcdab47
skalinin <kalinin.sa@phystech.edu>
2017-05-19 20:07:31 +0300

    Signed/unsigned s**t fix

--- kr_2/1/main.c
+++ kr_2/1/main.c
@@ -12,19 +12,7 @@
 
 const int long_bits = sizeof(long) * 8;
 
-void move_bits(long* a, int p_left, int p_right, int move_size)
-{
-    long mask_left, mask_right, n_left, n_right;
-    mask_left = (1l << (long_bits - p_left + 1)) - (1l << (long_bits - p_left - move_size + 1));
-    mask_right = (1l << (long_bits - p_right + 1)) - (1l << (long_bits - p_right - move_size + 1));
-    n_left = *a & mask_left;
-    n_right = *a & mask_right;
-    *a &= ~(mask_left | mask_right);
-    *a |= (n_left >> (p_right - p_left));
-    *a |= (n_right << (p_right - p_left));
-}
-
-void print_2(long a)
+void print_2(unsigned long a)
 {
     int i;
     for(i = long_bits - 1; i >= 0; --i)
@@ -32,6 +20,28 @@ void print_2(long a)
         printf("%d", (a & (1l << i)) > 0 ? 1 : 0);
     }
     puts("");
+}
+
+void move_bits(unsigned long* a, int p_left, int p_right, int move_size)
+{
+    unsigned long mask_left, mask_right, n_left, n_right;
+    if(p_left == 1)
+        mask_left = 1ul << (long_bits - 1);
+    else
+        mask_left = (1ul << (long_bits - p_left + 1)) - (1ul << (long_bits - p_left - move_size + 1));
+    
+    if(p_right == 1)
+        mask_right = 1ul << (long_bits - 1);
+    else
+        mask_right = (1ul << (long_bits - p_right + 1)) - (1ul << (long_bits - p_right - move_size + 1));
+    n_left = *a & mask_left;
+    n_right = *a & mask_right;
+    *a &= ~(mask_left | mask_right);
+    *a |= (n_left >> (p_right - p_left));
+    *a |= (n_right << (p_right - p_left));
+    print_2(mask_left);
+    print_2(mask_right);
+    print_2(~(mask_left | mask_right));
 }
 
 int main(int argc, const char* argv[])
@@ -64,7 +74,7 @@ int main(int argc, const char* argv[])
     {
         a = strtol(s, NULL, 16);
         print_2(a);
-        move_bits(&a, p_left, p_right, move_size);
+        move_bits((unsigned long *)&a, p_left, p_right, move_size);
         print_2(a);
         puts("");
         write(fd, &a, sizeof(long));
