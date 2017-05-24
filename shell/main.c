97376974482709fa1606a646e3768dea6e9c8e47
skalinin <kalinin.sa@phystech.edu>
2017-05-24 21:48:13 +0300

    That's something

---
+++ shell/main.c
@@ -0,0 +1,20 @@
+#define _XOPEN_SOURCE 1000
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+
+
+
+
+int main(int argc, const char* argv[])
+{
+    
+    return 0;
+}
